8cf25862a0320e55743233061610de491cb124bd
Shanidhya01 <luckykumar0011s@gmail.com>
2025-07-10 20:56:03 +0530

    Initial commit

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,12 +1,14 @@
+export const runtime = 'nodejs'; // 👈 This is required to fix the build issue on Vercel
+
 import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/sever/config";
 import React from "react";
 import EditQues from "./EditQues";
 
 const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
+  const question = await databases.getDocument(db, questionCollection, params.quesId);
 
-    return <EditQues question={question} />;
+  return <EditQues question={question} />;
 };
 
 export default Page;
